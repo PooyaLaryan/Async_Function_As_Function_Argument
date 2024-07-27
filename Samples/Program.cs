4ef13f12298828384e16328747fe35a0f3ec1d34
Lariyan.Pouya <Lariyan.Pouya@HIG001P4987>
2024-07-27 14:24:57 +0330

    Add project files.

---
+++ Samples/Program.cs
@@ -0,0 +1,16 @@
+﻿
+var b = await TestMethod(async x =>
+{
+    return await Task.FromResult(x+1);
+});
+
+Console.WriteLine(b);
+
+Console.ReadKey();
+
+async Task<int> TestMethod(Func<int, Task<int>> func)
+{
+    int a = 1;
+    var b = await func(a);
+    return b;
+}
